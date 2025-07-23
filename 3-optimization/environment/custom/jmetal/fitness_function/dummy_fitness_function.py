d9a26e7f9209d95614bc26dac7209299f36e1544
Carlos Benito Jareño <39310173+carlw0S@users.noreply.github.com>
2025-07-23 14:32:03 +0200

    Refactor DummyFitnessFunction to return a fixed fitness value instead of a random one

--- 3-optimization/environment/custom/jmetal/fitness_function/dummy_fitness_function.py
+++ 3-optimization/environment/custom/jmetal/fitness_function/dummy_fitness_function.py
@@ -1,4 +1,3 @@
-import random
 import time
 
 from custom.jmetal.fitness_function import FitnessFunction
@@ -13,25 +12,25 @@ from custom.jmetal.fitness_function import FitnessFunction
 class DummyFitnessFunction(FitnessFunction):
     """
     Dummy fitness function for testing purposes.
-    This fitness function simulates the evaluation of a solution by returning a random fitness value.
+    This fitness function simulates the evaluation of a solution by always returning the same fitness value.
 
     :param float delay: The delay in seconds to simulate processing time.
-    :param tuple fitness_range: A tuple representing the range of fitness values to return.
+    :param float fitness_value: The fixed fitness value to return.
     """
-    def __init__(self, delay: float = 1.0, fitness_range: tuple = (0.0, 100.0)):
+    def __init__(self, delay: float = 1.0, fitness_value: float = 13.0):
         super().__init__()
         self.delay = delay
-        self.fitness_range = fitness_range
+        self.fitness_value = fitness_value
 
     def calculate(self, solution_variables: list) -> float:
         """
-        Evaluate a solution by returning a random fitness value.
-        
+        Evaluate a solution by returning the fixed fitness value.
+
         :param list solution_variables: The variables of the solution to evaluate.
-        :return fitness_value: The evaluated solution with a random fitness value.
+        :return fitness_value: The evaluated solution with the fixed fitness value.
         """
         time.sleep(self.delay)
-        return random.uniform(*self.fitness_range)
-    
+        return self.fitness_value
+
     def name(self) -> str:
         return "Dummy Fitness Function"
